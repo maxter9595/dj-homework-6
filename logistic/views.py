c81b28f237121ce7022a219fa664c0dd00d6fb32
Maxim Terletskii <max.t95@bk.ru>
2024-07-10 21:03:57 +0300

    Corrections

--- logistic/views.py
+++ logistic/views.py
@@ -1,6 +1,9 @@
 from rest_framework import filters, status
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 from logistic.serializers import ProductSerializer, StockSerializer
 
 from logistic.models import Product, Stock, StockProduct
@@ -14,13 +17,25 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
 
     filter_backends = [
-        filters.SearchFilter
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter
+    ]
+
+    filterset_fields = [
+        'id',
     ]
 
     search_fields = [
         'title',
         'description'
     ]
+
+    ordering_fields = [
+        'id'
+    ]
+
+    pagination_class = LimitOffsetPagination
 
     def create(self, request, *args, **kwargs):
         """
@@ -59,12 +74,22 @@ class StockViewSet(ModelViewSet):
     serializer_class = StockSerializer
 
     filter_backends = [
-        filters.SearchFilter
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter,
     ]
 
     search_fields = [
-        'address'
+        'positions__product__id',
+        'positions__product__title',
+        'positions__product__description',
     ]
+
+    ordering_fields = [
+        'id'
+    ]
+
+    pagination_class = LimitOffsetPagination
 
     def create(self, request, *args, **kwargs):
         """
@@ -84,18 +109,44 @@ class StockViewSet(ModelViewSet):
         )
 
         for position in positions:
-            StockProduct.objects.create(
-                stock=stock,
-                product_id=position.get(
-                    'product'
-                ),
-                quantity=position.get(
-                    'quantity'
-                ),
-                price=position.get(
-                    'price'
-                )
+            product_id = position.get(
+                'product'
             )
+
+            product = Product.objects.filter(
+                id=product_id
+            )
+
+            if product.exists():
+                stock_product = StockProduct.objects.filter(
+                    product_id=product_id,
+                    stock=stock
+                )
+
+                if not stock_product.exists():
+                    StockProduct.objects.create(
+                        stock=stock,
+                        product_id=product_id,
+                        quantity=position.get(
+                            'quantity'
+                        ),
+                        price=position.get(
+                            'price'
+                        )
+                    )
+
+                else:
+                    return Response(
+                        {"error": "Stock with id {} already have the product with id {}".\
+                            format(stock.id, product_id)},
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
+
+            else:
+                return Response(
+                    {"error": "Product with id {} does not exist".format(product_id)},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
 
         serializer = StockSerializer(
             stock
@@ -106,87 +157,61 @@ class StockViewSet(ModelViewSet):
             status=status.HTTP_201_CREATED if created else status.HTTP_200_OK
         )
 
-    def partial_update(self, request, *args, **kwargs):
+    def partial_update(self, request, pk=None):
         """
         Используется для частичного обновления объекта Stock
         """
-        instance = self.get_object()
-        positions_data = request.data.get(
-            'positions', []
+        stock = Stock.objects.filter(
+            id=pk
         )
 
-        for position_data in positions_data:
-            product_id = position_data.get(
-                'product'
-            )
-            quantity = position_data.get(
-                'quantity'
-            )
-            price = position_data.get(
-                'price'
+        if stock.exists():
+            instance = stock.first()
+
+            positions_data = request.data.get(
+                'positions', []
             )
 
-            try:
-                stock_product = StockProduct.objects.get(
-                    stock=instance,
-                    product_id=product_id
+            for position_data in positions_data:
+                product_id = position_data.get(
+                    'product'
+                )
+                quantity = position_data.get(
+                    'quantity'
+                )
+                price = position_data.get(
+                    'price'
                 )
 
-                stock_product.quantity = quantity
-                stock_product.price = price
-                stock_product.save()
+                try:
+                    stock_product = StockProduct.objects.get(
+                        stock=instance,
+                        product_id=product_id
+                    )
 
-            except StockProduct.DoesNotExist:
-                StockProduct.objects.create(
-                    stock=instance,
-                    product_id=product_id,
-                    quantity=quantity,
-                    price=price
-                )
+                    stock_product.quantity = quantity
+                    stock_product.price = price
+                    stock_product.save()
 
-        serializer = StockSerializer(
-            instance
-        )
+                except StockProduct.DoesNotExist:
+                    StockProduct.objects.create(
+                        stock=instance,
+                        product_id=product_id,
+                        quantity=quantity,
+                        price=price
+                    )
 
-        return Response(
-            serializer.data,
-            status=status.HTTP_200_OK
-        )
-
-    def list(self, request, *args, **kwargs):
-        """
-        Возвращает список объектов Stock, отфильтрованный по одному из двух вариантов:
-        - по параметру products__id (если указан параметр ?products=... в GET-запросе)
-        - по названию или описанию продукта (если указан параметр ?search=... в GET-запросе)
-        """
-        product_id = request.query_params.get(
-            'products',
-        )
-
-        product_query = request.query_params.get(
-            'search',
-        )
-
-        if product_id:
-            stocks = Stock.objects.filter(
-                products__id=product_id
+            serializer = StockSerializer(
+                instance
             )
 
-        elif product_query:
-            stocks = Stock.objects.filter(
-                positions__product__title__icontains=product_query
-            ) | Stock.objects.filter(
-                positions__product__description__icontains=product_query
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
             )
 
         else:
-            return super().list(request, *args, **kwargs)
-
-        serializer = StockSerializer(
-            stocks,
-            many=True
-        )
-
-        return Response(
-            serializer.data
-        )
+            return Response(
+                'Stock not found',
+                status=status.HTTP_404_NOT_FOUND
+            )
